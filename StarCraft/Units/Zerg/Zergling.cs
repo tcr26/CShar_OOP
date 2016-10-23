0f83e018f6b6e25365d3304e84179defb3e51a9b
tcr26 <dcrow@abv.bg>
2016-10-23 22:16:18 +0300

    Zergling

---
+++ StarCraft/Units/Zerg/Zergling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarCraft.Units.Zerg
+{
+    class Zergling : BaseUnit
+    {
+        public Zergling(string objectName, int hp, int armorPoints, int construction, string rank, int movementSpeed)
+            : base(objectName, hp, armorPoints, construction, rank, movementSpeed)
+        {
+        }
+    }
+}
